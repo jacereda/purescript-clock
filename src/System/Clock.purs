d3ce8b394ce63199ac7f4bc36eae0960fedf2d72
Sergio Acereda <sacereda@users.noreply.github.com>
2017-04-21 16:52:56 +0200

    Explicit imports

--- src/System/Clock.purs
+++ src/System/Clock.purs
@@ -1,16 +1,15 @@
 module System.Clock where
 
-import Prelude
-
-import Data.UInt (UInt, round)
 import Control.Monad.Eff (kind Effect, Eff)
+import Data.Ring ((-))
+import Data.UInt (UInt, round)
 
 foreign import data CLOCK :: Effect
 
 newtype Stamp = Stamp Number
 type Nanoseconds = UInt
 
-foreign import now :: forall e. Eff (clock :: CLOCK|e) Stamp
+foreign import now :: forall e. Eff (clock :: CLOCK | e) Stamp
 
 interval :: Stamp -> Stamp -> Nanoseconds
 interval (Stamp a) (Stamp b) = round (b - a)
