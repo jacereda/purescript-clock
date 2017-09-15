66d40a170998ba16557dc6c92b30507c80c1f890
Jorge Acereda <jacereda@gmail.com>
2017-09-15 17:57:47 +0200

    Return times relative to the app start in node

--- src/System/Clock.js
+++ src/System/Clock.js
@@ -1,13 +1,17 @@
 "use strict";
-
-// module System.Clock
 var p = typeof window != 'undefined' && window.performance || {}
 var pr = typeof global != 'undefined' && global.process || {}
-exports.nanoseconds = p.now && function() { return p.now() * 1e6; }
-    || p.mozNow && function() { return p.mozNow() * 1e6; }
-    || p.msNow && function() { return p.msNow() * 1e6; }
-    || p.oNow && function() { return p.oNow() * 1e6; }
-    || p.webkitNow && function() { return p.webkitNow() * 1e6; }
-    || pr.hrtime && function() { var t = pr.hrtime(); return t[0] * 1e9 + t[1]; }
-    || Date.now && function() { return Date.now() * 1e6; }
-    || function () { return new Date().getTime() * 1e6; };
+var raw = false
+    || pr.hrtime
+    || p.now && function() { return p.now(); }
+    || p.mozNow && function() { return p.mozNow(); }
+    || p.msNow && function() { return p.msNow(); }
+    || p.oNow && function() { return p.oNow(); }
+    || p.webkitNow && function() { return p.webkitNow(); }
+    || Date.now
+    || function () { return new Date().getTime(); };
+var ns0 = raw();
+exports.nanoseconds =
+  raw === pr.hrtime && function() { var t = raw(ns0); return t[0] * 1e9 + t[1]; }
+  || p && function() { return raw() * 1e6; }
+  || function() { return (raw()-ns0) * 1e6; };
